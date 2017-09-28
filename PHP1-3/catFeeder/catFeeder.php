3cc516bfe74e40636fadd74f80a7ea72ec2c1148
Alex <alex@xpresstek.net>
2017-09-27 21:09:32 -0400

    Lesson 3 exercises

--- PHP1-3/catFeeder/catFeeder.php
+++ PHP1-3/catFeeder/catFeeder.php
@@ -1,10 +1,20 @@
 <?php
+require_once("catFeederValidator.php");
 
 function handleRemainder($remainder) {
 
 }
 
 function dispenceFood($initialAmount, $portionSize) {
+
+    $is_valid = validateInitialAmount($initialAmount) &&
+                validatePortionSize($portionSize, $initialAmount);
+
+    if(!$is_valid) {
+        echo "Validation failed!";
+        return;
+    }
+
     $portion = 0;
     $remainder = 0;
 
--- PHP1-3/catFeeder/catFeederTest.php
+++ PHP1-3/catFeeder/catFeederTest.php
@@ -1,36 +1,82 @@
 <?php
-include_once('catFeeder.php');
+require_once 'catFeeder.php';
+require_once "catFeederValidator.php";
 
-function testDispenceFood() {
+function testDispenceFood()
+{
 
     $initalAmount = 100;
     $portionSize = 5;
 
     $result = dispenceFood($initialAmount, $portionSize);
 
-    if($result == 5) {
+    if ($result == 5) {
         echo "testDispenceFood: Test Passed!";
     } else {
         echo "testDispenceFood: Test Failed! Expected 5 but received $result";
     }
 }
 
-function testAddFood() {
+function testAddFood()
+{
     $initialAmount = 100;
     $amountToAdd = 10;
 
     $result = addFood($initialAmount, $amountToAdd);
-    
-    if($result == 110) {
-            echo "testAddFood: Test Passed!";
-        } else {
-            echo "testAddFood: Test Failed! Expected 110 but received $result";
-        }
+
+    if ($result == 110) {
+        echo "testAddFood: Test Passed!";
+    } else {
+        echo "testAddFood: Test Failed! Expected 110 but received $result";
+    }
 
 }
 
-testDispenceFood();
-echo "<br />";
-testAddFood();
+function testValidateInitialAmount($initialAmount)
+{
+    foreach ($initialAmount as $key => $value) {
+        $result = validateInitialAmount($key);
+
+        if ($result != $value) {
+            echo "<br />testValidateInitialAmount $key: Test Failed!
+        Expected $value but received $result<br />";
+        } else {
+            echo "<br />testValidateInitialAmount: Test Passed!<br />";
+        }
+    }
+}
+
+function testValidatePortionSize($testKeyValuePairs, $intialSize) {
+    foreach ($testKeyValuePairs as $key => $value) {
+        $result = validatePortionSize($key, $initialSize);
+        //echo "$key : $value : $intialSize : $result";
+
+        if ($result != $value) {
+            echo "<br />testValidatePortionSize $key: Test Failed!
+        Expected $value but received $result<br />";
+        } else {
+            echo "<br />testValidatePortionSize: Test Passed!<br />";
+        }
+    }
+}
+
+$a = array();
+
+$keyValuePairs =  array(
+    $a => false,
+    "foo" => false, 
+    0 => false, 
+    -1 => false,
+    20 => true
+);
+
+$keyValuePairs2 =  array(
+    100 => false
+);
+
+testValidateInitialAmount($keyValuePairs);
+
+testValidatePortionSize($keyValuePairs, 250);
+testValidatePortionSize($keyValuePairs2, 50);
 
 ?>
