a7e03d75d281bcc49d5b61cda7c9421789dedb15
Alex <alex@xpresstek.net>
2017-10-03 21:07:42 -0400

    PHP 2 Lesson 1

--- PHP1-3/classTest/Dog.php
+++ PHP1-3/classTest/Dog.php
@@ -2,18 +2,42 @@
 
 class Dog
 {
+    const NUMBER_OF_PAWS = 4;
+    static $is_tame = true;
+
     private $name;
     private $isSittingDown;
+    private $color;
 
-    public function __construct($name)
+    public function __construct($name, $color)
     {
         $this->name = $name;
         $this->isSittingDown = false;
+        $this->color = $color;
     }
 
     public function __destruct()
     {
         //echo "<br />Bye bye, $this->name is going away";
+    }
+
+    public function whatColor()
+    {
+        echo "<br /> I am $this->color";
+    }
+
+    public static function isTame()
+    {
+        if (self::$is_tame) {
+            echo "<br />I'm tame!<br />";
+        } else {
+            echo "<br />'m feral!<br />";
+        }
+    }
+
+    public function paws()
+    {
+        echo "I have " . self::NUMBER_OF_PAWS . " paws";
     }
 
     public function bark()
@@ -42,14 +66,14 @@ class Dog
     }
 }
 
-$dog = new Dog("Fido");
-$dog->bark();
+class YellowLab extends Dog
+{
+    public function __construct($name)
+    {
+        parent::__construct($name, "yellow");
+    }
+}
 
-$puppy = new Dog("Rover");
-$puppy->bark();
-
-$puppy->sit();
-$dog->sit("Milk bone");
-
-$puppy->isSittingDown();
-$dog->isSittingDown();
+$fido = new YellowLab("Fido");
+$fido->bark();
+$fido->whatColor();
